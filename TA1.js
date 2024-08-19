b33e3bd778224b28a4ed389b1b9c8b3128ba6ca6
Cristianbaumann <cristian.baumann@correo.ucu.edu.uy>
2024-08-19 16:41:36 -0300

    TAs

--- TA1.js
+++ TA1.js
@@ -30,9 +30,9 @@ console.log(removeFromArray([1,2,3,4,5,6,7,8,9],2));
 
 function getTheTitles(libros) {
   const titulos = libros.map(libro => libro.title);
-  const contenedor = document.getElementById('ContendeorDeH1');
+  const contenedor = document.getElementById("ContendeorDeH1");
   titulos.forEach(titulo => {
-    const h1 = document.createElement('h1');
+    const h1 = document.createElement("h1");
     h1.textContent = titulo;
     contenedor.appendChild(h1);
   });
@@ -48,14 +48,44 @@ function mostrarTitulos() {
   getTheTitles(libros);
 }
 
+function getOdds(nums){
+  const odds = [];
+  let output = "";
+  for(let i = 0; i<= nums.length; i++){
+    if(i % 2 !== 0){
+      odds.push(i)
+    } 
+  }
+  odds.forEach((odds) => {
+    output += `<span>${odds}</span>`;
+  });
+  document.getElementById("ContenedorDeOdds").innerHTML = output;
+}
 
+function duplicates(nums) {
+  const counts = {}; 
+  const duplicates = {};
+  const contenedor = document.getElementById("duplicatesContenedor");
 
-    
+  nums.forEach(num => {
+    counts[num] = (counts[num] || 0) + 1;
+  });
+  for (let num in counts) {
+    if (counts[num] > 1) {
+      duplicates[num] = counts[num];
+    }
+  }
 
-
-
-
-
+  for (let num in duplicates) {
+    const h4 = document.createElement("h4");
+    h4.textContent = `Valor duplicado: ${num}`;
+  
+    const p = document.createElement("p");
+    p.textContent = `Aparece ${duplicates[num]} veces`;
+    contenedor.appendChild(h4);
+    contenedor.appendChild(p);
+  }
+}
 function generarTexto() {
     const texto = document.getElementById("texto").value;
     const repeticiones = parseInt(document.getElementById("repeticiones").value);
---
+++ TA2.js
@@ -0,0 +1,37 @@
+function sumAll(){
+    const Pnumero = parseInt(document.getElementById("NumeroInicio").value);
+    const Unumero = parseInt(document.getElementById("NumeroFinal").value);
+    let suma = 0;
+    for(let i = Pnumero; i<= Unumero; i++){
+        suma += i
+    }
+    document.getElementById("resutado").innerText = `La suma de los números entre ${Pnumero} y ${Unumero} es: ${suma}`;
+}
+
+function convertToCelsius(){
+    const temp = parseInt(document.getElementById("temperatura").value);
+    const tocelcius =(((temp - 32)*5)/9).toFixed(1);
+    document.getElementById("Conversion").innerText= `${temp}°f son ${tocelcius} celsius`;
+}
+
+function convertToFahrenheit(){
+    const temp = parseInt(document.getElementById("temperatura").value);
+    const toFahrenheit = ((temp * 9) / 5 + 32).toFixed(1);
+    document.getElementById("Conversion").innerText= `${temp}°f son ${toFahrenheit} celsius`;
+}
+
+function leapYear(){
+    const year = parseInt(document.getElementById("bisiesto").value);
+    let check = ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0);
+    document.getElementById("TrueOrFalse").innerText= `${year} es biciesto? ${check}`;
+}
+
+function getSum(array){
+    let sum = 0;
+    for(let i =0; i< array.length; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+arr = [1,2,3]
+console.log(getSum(arr));
